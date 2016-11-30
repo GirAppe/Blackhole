f8da41a8ea646816db0e3eb25752aa5d0cfcab92
Andrzej Michnia <amichnia@girappe.com>
2016-11-30 11:56:48 +0100

    Working watch example app.

--- Example/Blackhole/ViewController.swift
+++ Example/Blackhole/ViewController.swift
@@ -9,6 +9,23 @@
 import UIKit
 import Blackhole
 
+enum CatBreed : String {
+    case bengal = "bengal"
+    case persian = "persian"
+    case siamese = "siamese"
+    
+    var image: UIImage {
+        switch self {
+        case .bengal:
+            return #imageLiteral(resourceName: "bengal")
+        case .persian:
+            return #imageLiteral(resourceName: "persian")
+        case .siamese:
+            return #imageLiteral(resourceName: "siamese")
+        }
+    }
+}
+
 class ViewController: UIViewController {
     // MARK: - Outlets
     @IBOutlet weak var catImageView: UIImageView!
@@ -26,11 +43,47 @@ class ViewController: UIViewController {
         
     }
     
+    // MARK: - Private
+    private func validate() -> Bool {
+        return (catNameTextField.text?.characters.count ?? 0) > 0
+    }
+    
+    private func revalidate() {
+        sendCatButton.isEnabled = validate()
+    }
+    
     // MARK: - Actions
+    @IBAction func nameChangedAction(_ sender: UITextField) {
+        revalidate()
+    }
+    
+    @IBAction func nameEditEndedAction(_ sender: UITextField) {
+        revalidate()
+    }
+    
+    @IBAction func catBreedSelected(_ sender: UISegmentedControl) {
+        switch sender.selectedSegmentIndex {
+        case 0:
+            catImageView.image = CatBreed.bengal.image
+        case 1:
+            catImageView.image = CatBreed.persian.image
+        case 2:
+            catImageView.image = CatBreed.siamese.image
+        default:
+            break
+        }
+    }
+    
     @IBAction func sendCatAction(_ sender: AnyObject) {
+        
+    }
+    
+    @IBAction func dismissKeyboardAction(_ sender: Any) {
+        catNameTextField.resignFirstResponder()
+        revalidate()
     }
     
     // MARK: - Navigation
-
+    
 }
 
